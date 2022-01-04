4eb2d9b2058f8c2c900b1cf3d4458ccd26567091
SiravitPhokeed <siravitsasa@gmail.com>
2022-01-04 14:09:18 +0700

    feat: Working time left progress bar

--- src/components/dashboard/current-class.tsx
+++ src/components/dashboard/current-class.tsx
@@ -1,8 +1,174 @@
+// Modules
+import {
+  addMinutes,
+  formatDuration,
+  intervalToDuration,
+  set
+} from "date-fns";
+
 // Components
 import Headline from "@components/global/headline";
 import MaterialIcon from "@components/global/icon";
+import { useEffect, useState } from "react";
+import { SubjectPeriod } from "@utils/types/class";
 
-const CurrentClass = () => {
+/**
+ * Ongoing class infobox
+ * @param instructors An array of instructors for this class
+ * @param periodStart The start time of this class
+ * @param periodLength The length of the period
+ * @param dueNumber The number of assignments due this period
+ */
+const ClassInfo = ({
+  instructors,
+  periodStart,
+  periodLength,
+  dueNumber,
+}: {
+  instructors: Array<{ name: string }>;
+  periodStart: Date;
+  periodLength: number;
+  dueNumber: number;
+}): JSX.Element => {
+  return (
+    <div className="flex flex-col gap-3 px-4 pb-4">
+      <Headline
+        icon={
+          <MaterialIcon
+            icon="person"
+            className="text-light-primary dark:text-dark-primary"
+          />
+        }
+        title={<h3 className="text-base">Instructor</h3>}
+        subhead={
+          <ul className="text-xl">
+            <li>Dr John Peter Smith</li>
+            <li>Mattana Tatanyang</li>
+            {instructors.map((instructor) => {
+              <li>{instructor.name}</li>;
+            })}
+          </ul>
+        }
+        className="p-0"
+      />
+      <Headline
+        icon={
+          <MaterialIcon
+            icon="schedule"
+            className="text-light-primary dark:text-dark-primary"
+          />
+        }
+        title={<h3 className="text-base">Time</h3>}
+        subhead={<p className="text-xl">14:20-16:00</p>}
+        className="p-0"
+      />
+      <Headline
+        icon={
+          <MaterialIcon
+            icon="assignment"
+            className="text-light-primary dark:text-dark-primary"
+          />
+        }
+        subhead={<p className="text-xl">2 assignments due</p>}
+        className="p-0"
+      />
+    </div>
+  );
+};
+
+/**
+ * Displays both text and progress bar representation of the passage of time within a period.
+ *
+ * Teachers may dislike this...
+ * @param periodStart The start time of this class
+ * @param periodLength The length of the period
+ */
+const ClassTimeLeft = ({
+  periodStart,
+  periodLength,
+}: {
+  periodStart: Date;
+  periodLength: number;
+}): JSX.Element => {
+  // Add minutes to the start time to get the end time
+  const periodEnd = addMinutes(periodStart, periodLength);
+
+  // How much time is left until the period ends
+  const [timeLeft, setTimeLeft] = useState<Duration>(
+    intervalToDuration({
+      start: new Date(),
+      end: periodEnd,
+    })
+  );
+
+  // Updates timeLeft every second
+  useEffect(() => {
+    const clock = setInterval(
+      () =>
+        setTimeLeft(
+          intervalToDuration({
+            start: new Date(),
+            end: periodEnd,
+          })
+        ),
+      1000
+    );
+
+    return () => {
+      clearInterval(clock);
+    };
+  }, []);
+
+  return (
+    <div className="p-4">
+      <p>{formatDuration(timeLeft)} left</p>
+      <progress
+        value={
+          periodLength -
+          ((timeLeft.hours || 0) * 60 + (timeLeft.minutes || periodLength) + (timeLeft.seconds || 0) * (0.0166))
+        }
+        max={periodLength}
+        className="progress"
+      />
+    </div>
+  );
+};
+
+const ClassActions = ({
+  ggc,
+  meet,
+}: {
+  ggc: string;
+  meet: string;
+}): JSX.Element => {
+  return (
+    <div className="flex flex-row justify-end gap-2 p-4">
+      <a
+        href={`https://classroom.google.com/u/c/${ggc}`}
+        target="_blank"
+        className="btn btn-outlined"
+      >
+        Classroom
+      </a>
+      <a href={meet} target="_blank" className="btn btn-filled">
+        <MaterialIcon icon="videocam" />
+        Join Meet
+      </a>
+    </div>
+  );
+};
+
+/**
+ * Displays at-a-glance information about the current ongoing class
+ *
+ * We suspect users will just stare at the progress bar, though
+ * @param subjectPeriod An instance of SubjectPeriod
+ */
+const CurrentClass = ({
+  subjectPeriod,
+}: {
+  subjectPeriod: SubjectPeriod;
+}): JSX.Element => {
   return (
     <section
       className="w-full p-3 text-light-on-surface bg-light-surface2
@@ -13,71 +179,29 @@ const CurrentClass = () => {
           <h2 className="text-xl">Current Class</h2>
           <h3 className="text-5xl font-bold">English 8</h3>
         </div>
-        <div className="flex flex-col gap-3 px-4 pb-4">
-          <Headline
-            icon={
-              <MaterialIcon
-                icon="person"
-                className="text-light-primary dark:text-dark-primary"
-              />
-            }
-            title={<h3 className="text-base">Instructor</h3>}
-            subhead={
-              <ul className="text-xl">
-                <li>Dr John Peter Smith</li>
-                <li>Mattana Tatanyang</li>
-              </ul>
-            }
-            className="p-0"
-          />
-          <Headline
-            icon={
-              <MaterialIcon
-                icon="schedule"
-                className="text-light-primary dark:text-dark-primary"
-              />
-            }
-            title={<h3 className="text-base">Time</h3>}
-            subhead={<p className="text-xl">14:20-16:00</p>}
-            className="p-0"
-          />
-          <Headline
-            icon={
-              <MaterialIcon
-                icon="assignment"
-                className="text-light-primary dark:text-dark-primary"
-              />
-            }
-            subhead={<p className="text-xl">2 assignments due</p>}
-            className="p-0"
-          />
-        </div>
+        <ClassInfo
+          instructors={[
+            { name: "Dr John Peter Smith" },
+            { name: "Mattana Tatanyang" },
+          ]}
+          periodStart={set(new Date(), { hours: 14, minutes: 20, seconds: 0 })}
+          periodLength={100}
+          dueNumber={2}
+        />
       </div>
       <div className="flex flex-col sm:grid sm:grid-cols-2 gap-3">
-        <div className="p-4">
-          <p>1 hour 12 minutes left</p>
-          <progress value={32} max={100} className="progress" />
-        </div>
-        <div className="flex flex-row justify-end gap-2 p-4">
-          <a
-            href="https://classroom.google.com/u/1/c/MzQzNTA0NDkyMjgx"
-            target="_blank"
-            className="btn btn-outlined"
-          >
-            Classroom
-          </a>
-          <a
-            href="https://meet.google.com/zbm-wnny-fsi"
-            target="_blank"
-            className="btn btn-filled"
-          >
-            <MaterialIcon icon="videocam" />
-            Join Meet
-          </a>
-        </div>
+        <ClassTimeLeft
+          periodStart={set(new Date(), { hours: 14, minutes: 20, seconds: 0 })}
+          periodLength={100}
+        />
+        <ClassActions
+          ggc="MzQzNTA0NDkyMjgx"
+          meet="https://meet.google.com/zbm-wnny-fsi"
+        />
       </div>
     </section>
   );
 };
 
+// Exports
 export default CurrentClass;
---
+++ src/utils/types/class.ts
@@ -0,0 +1,5 @@
+export type SubjectPeriod = {
+  name: string,
+  instructors: Array<string>,
+  // TODO
+}
