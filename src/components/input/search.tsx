789d74487be753754e69c5e0041007df3fb6c78e
SiravitPhokeed <siravitsasa@gmail.com>
2022-01-10 15:54:41 +0700

    feat: Replaced default outline

--- src/components/input/search.tsx
+++ src/components/input/search.tsx
@@ -24,8 +24,8 @@ const Search = ({
   return (
     <div className="px-4 pt-4 pb-2">
       <div
-        className="flex flex-row justify-between items-center w-full h-12 container-surface overflow-hidden shadow
-        rounded-full"
+        className="flex flex-row justify-between items-center w-full h-12 container-surface
+        overflow-hidden shadow focus-visible:ring-0 rounded-full"
       >
         <button
           className="btn-text grid place-items-center h-full px-4 text-light-on-surface dark:text-dark-on-surface"
@@ -38,10 +38,10 @@ const Search = ({
           <MaterialIcon icon="search" />
         </button>
         <input
-          className="block w-full h-full container-surface outline-none
-          placeholder:text-light-on-surface placeholder:dark:text-dark-on-surface
-          hover:transition-none hover:bg-light-primary-0.08-tlc hover:dark:bg-dark-primary-0.08-tlc
-          focus:placeholder:text-light-on-surface-variant  focus:placeholder:dark:text-dark-on-surface-variant"
+          className="block w-full h-full container-surface focus-visible:ring-0
+            placeholder:text-light-on-surface placeholder:dark:text-dark-on-surface
+            hover:transition-none hover:bg-light-primary-0.08-tlc hover:dark:bg-dark-primary-0.08-tlc
+            focus:placeholder:text-light-on-surface-variant focus:placeholder:dark:text-dark-on-surface-variant"
           onChange={(e) => {
             if (onChange) {
               onChange(e.target.value);
@@ -51,7 +51,10 @@ const Search = ({
           placeholder={placeholder || "Search"}
           role="search"
         />
-        <button className="btn-text grid place-items-center h-full px-4 text-light-on-surface dark:text-dark-on-surface">
+        <button
+          className="btn-text grid place-items-center h-full px-4
+          text-light-on-surface dark:text-dark-on-surface focus-visible:ring-0"
+        >
           <MaterialIcon icon="filter_list" />
         </button>
       </div>
