777f843e3d7ce466df6f1f457be82d50d5614f54
SiravitPhokeed <siravitsasa@gmail.com>
2022-01-12 22:49:55 +0700

    refactor: Bottom sheet uses state

--- src/components/schedule/period-info.tsx
+++ src/components/schedule/period-info.tsx
@@ -13,70 +13,73 @@ import { SubjectPeriod } from "@utils/types/subject/period";
  */
 const PeriodInfo = ({ subjectPeriod }: { subjectPeriod: SubjectPeriod }) => {
   return (
-    <section
-      className="bg-light-surface1 dark:bg-dark-surface1 p-4 sm:mt-7 sm:mx-7 sm:rounded-t-lg overflow-hidden"
-      aria-live="assertive"
-    >
-      <div className="flex flex-row justify-between items-start">
-        <h2 className="text-4xl font-bold">{subjectPeriod.subject.enName.name}</h2>
-        <button className="btn-text flex p-2 rounded-full">
-          <MaterialIcon
-            icon="close"
-            className="text-light-on-surface dark:text-dark-on-surface"
-          />
-        </button>
-      </div>
-      <div className="h-48 overflow-auto sm:h-fit sm:overflow-hidden">
-        <div className="flex flex-col gap-4 sm:grid sm:grid-cols-2">
-          <div className="pt-2">
-            <h3 className="sr-only">Class Information</h3>
-            <ClassInfo
-              instructors={subjectPeriod.subject.instructors}
-              periodStart={subjectPeriod.periodStart}
-              periodLength={subjectPeriod.periodLength}
-              dueNumber={subjectPeriod.assignmentsDue.length}
-              noPadding={true}
+    <div className="flex flex-row justify-center w-full">
+      <section
+        className="fixed bottom-16 p-4 w-[calc(100%-1rem)] bg-light-surface1 dark:bg-dark-surface1
+        lg:w-3/4 sm:bottom-0 sm:w-[calc(100%-6rem)] sm:rounded-t-lg overflow-hidden"
+        aria-live="assertive"
+      >
+        <div className="flex flex-row justify-between items-start">
+          <h2 className="text-4xl font-bold">{subjectPeriod.subject.enName.name}</h2>
+          <button className="btn-text flex p-2 rounded-full">
+            <MaterialIcon
+              icon="close"
+              className="text-light-on-surface dark:text-dark-on-surface"
             />
-          </div>
-          <div className="card-tonal flex flex-col rounded-t-lg">
-            <Title
-              icon={
-                <MaterialIcon
-                  icon="link"
-                  className="text-light-primary dark:text-dark-primary"
-                />
-              }
-              title={<h4>Related Posts</h4>}
-            />
-            <div className="h-40 px-4 overflow-auto scroll-1">
-              <ul className="flex flex-col gap-2 pb-4">
-                <MinifiedPost
-                  post={{
-                    author: "Tempoom L.",
-                    postDate: new Date(2022, 0, 1, 0, 8, 39),
-                    attachments: [],
-                  }}
-                />
-                <MinifiedPost
-                  post={{
-                    author: "Sirawish S.",
-                    postDate: new Date(2021, 11, 30, 14, 16, 3),
-                    attachments: [],
-                  }}
-                />
-                <MinifiedPost
-                  post={{
-                    author: "Smart W.",
-                    postDate: new Date(2021, 11, 25, 9, 19, 58),
-                    attachments: [],
-                  }}
-                />
-              </ul>
+          </button>
+        </div>
+        <div className="h-48 overflow-auto sm:h-fit sm:overflow-hidden">
+          <div className="flex flex-col gap-4 sm:grid sm:grid-cols-2">
+            <div className="pt-2">
+              <h3 className="sr-only">Class Information</h3>
+              <ClassInfo
+                instructors={subjectPeriod.subject.instructors}
+                periodStart={subjectPeriod.periodStart}
+                periodLength={subjectPeriod.periodLength}
+                dueNumber={subjectPeriod.assignmentsDue.length}
+                noPadding={true}
+              />
+            </div>
+            <div className="card-tonal flex flex-col rounded-t-lg">
+              <Title
+                icon={
+                  <MaterialIcon
+                    icon="link"
+                    className="text-light-primary dark:text-dark-primary"
+                  />
+                }
+                title={<h4>Related Posts</h4>}
+              />
+              <div className="h-40 px-4 overflow-auto scroll-1">
+                <ul className="flex flex-col gap-2 pb-4">
+                  <MinifiedPost
+                    post={{
+                      author: "Tempoom L.",
+                      postDate: new Date(2022, 0, 1, 0, 8, 39),
+                      attachments: [],
+                    }}
+                  />
+                  <MinifiedPost
+                    post={{
+                      author: "Sirawish S.",
+                      postDate: new Date(2021, 11, 30, 14, 16, 3),
+                      attachments: [],
+                    }}
+                  />
+                  <MinifiedPost
+                    post={{
+                      author: "Smart W.",
+                      postDate: new Date(2021, 11, 25, 9, 19, 58),
+                      attachments: [],
+                    }}
+                  />
+                </ul>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </section>
+      </section>
+    </div>
   );
 };
 
