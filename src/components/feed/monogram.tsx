b1ce84d838ab450657cd1d35a07ccc0f3ccd4aaf
SiravitPhokeed <siravitsasa@gmail.com>
2022-01-19 17:09:22 +0700

    fix: Disabled user select on monogram

--- src/components/feed/monogram.tsx
+++ src/components/feed/monogram.tsx
@@ -2,7 +2,7 @@ const Monogram = ({ name }: { name: string }) => {
   return (
     <div
       className="grid place-items-center w-10 h-10 aspect-square
-        text-light-on-secondary font-medium bg-light-secondary rounded-full"
+        text-light-on-secondary font-medium bg-light-secondary rounded-full [user-select:none]"
     >
       {name.length > 0 ? name[0].toUpperCase() : ":)"}
     </div>
